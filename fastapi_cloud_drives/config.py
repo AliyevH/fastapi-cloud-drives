9af388fd95d0e5dbaadbcf482abc638292a3cce8
Sabuhi <sabuhi.shukurov@gmail.com>
2021-02-19 17:23:15 +0400

    change config class adding new features

--- fastapi_cloud_drives/config.py
+++ fastapi_cloud_drives/config.py
@@ -1,10 +1,25 @@
-from pydantic import BaseModel
+from pydantic import BaseSettings, Field, validator
 from typing import List
 from pathlib import Path
+from .errors import AutherizeGoogleClient
+import os
+
+class GoogleDriveConfig(BaseSettings):
+    CLIENT_ID_JSON: str = Field(..., env='CLIENT_ID_JSON')
+    SCOPES: List[str] = Field(..., env='SCOPES')
+    STORAGE_JSON: Path = Field(None, env='STORAGE_JSON')
+
+    class Config:
+        case_sensitive = True
+    
+
+    @validator("STORAGE_JSON")
+    def validate_name(cls,v):
+        if not v and  os.path.exists("storage.json"):
+            return v
+        raise AutherizeGoogleClient("File for authorizing does not exists")
 
 
-class GoogleDriveConfig(BaseModel):
-    CLIENT_ID_JSON: str
-    SCOPES: List[str]
-    STORAGE_JSON: Path = None
 
+def validate_path(path):
+    cur_dir = os.path.abspath(os.curdir)
