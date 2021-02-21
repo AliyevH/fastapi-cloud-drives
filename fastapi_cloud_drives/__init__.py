5f52bfa7d0146775e4937d8f402b1dd190682c1a
Sabuhi <sabuhi.shukurov@gmail.com>
2021-02-21 16:17:57 +0400

    provided examples for dropbox closes #23

--- fastapi_cloud_drives/__init__.py
+++ fastapi_cloud_drives/__init__.py
@@ -1,10 +1,13 @@
 from fastapi_cloud_drives.fastapi_google import GoogleDrive
+from fastapi_cloud_drives.fastapi_dropbox import DropBox
+
 from fastapi_cloud_drives.config import GoogleDriveConfig
+from fastapi_cloud_drives.config import DropBoxConfig
 
 from fastapi_cloud_drives.fastapi_s3 import S3
 
 __author__ = "hasan.aliyev.555@gmail.com"
 
 __all__ = [
-    "GoogleDrive", "GoogleDriveConfig"
+    "GoogleDrive", "GoogleDriveConfig", "DropBoxConfig", "DropBox"
 ]
