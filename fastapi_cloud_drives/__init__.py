13551b4864ac9f2257dc7ad62ba5e7dcd69df25d
Olawale <folarinolawale3415@gmail.com>
2021-08-23 07:53:20 +0100

    S3 import fix

--- fastapi_cloud_drives/__init__.py
+++ fastapi_cloud_drives/__init__.py
@@ -1,13 +1,13 @@
 from fastapi_cloud_drives.fastapi_google import GoogleDrive
 from fastapi_cloud_drives.fastapi_dropbox import DropBox
+from fastapi_cloud_drives.fastapi_s3 import S3
 
 from fastapi_cloud_drives.config import GoogleDriveConfig
 from fastapi_cloud_drives.config import DropBoxConfig
-
-from fastapi_cloud_drives.fastapi_s3 import S3
+from fastapi_cloud_drives.config import S3Config
 
 __author__ = "hasan.aliyev.555@gmail.com"
 
 __all__ = [
-    "GoogleDrive", "GoogleDriveConfig", "DropBoxConfig", "DropBox"
+    "S3", "S3Config", "GoogleDrive", "GoogleDriveConfig", "DropBoxConfig", "DropBox"
 ]
